7e540cc914844774f83fe4ea1d3626a3e42097a5
Shodmonov01 <a.shodmonov60@gmail.com>
2024-02-12 21:26:40 +0500

    footer chala

--- Frontend/src/components/Footer/Footer.tsx
+++ Frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,36 @@
-const Footer = () => {
-  return <footer>Footer</footer>;
-};
+import minilogo from '../../assets/images/mini-logo.png'
+function Footer() {
+  return (
+    <>
+      <footer className="container_xxl bg-black">
+        <div className="text-white flex justify-around
+        ">
+          <div className="w-1/3">
+            <p>По всем вопросам</p>
+            <h1>maldex @info.com</h1>
+          </div>
+          <div className="w-1/3">
+            <p>По всем вопросам</p>
+            <h1>maldex @info.com</h1>
+          </div>
+          <div className="w-1/12">
+            {/* <p>По всем вопросам</p>
+            <h1>maldex @info.com</h1> */}
+            <img src={minilogo} alt="" />
+          </div>
+        </div>
+
+
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </footer>
+    </>
+  );
+}
 
 export default Footer;
--- Frontend/src/components/Navbar/Navbar.tsx
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -5,98 +5,87 @@ import location from "../../assets/icons/location.svg";
 
 const Navbar = () => {
   return (
-    <>
-      <header className="container_xxl ">
-        <nav className="  py-4 flex justify-around items-center bg-slate-100 border-b border-gray-300">
-          {/* Logo and Phone Number */}
-          <div className="flex items-center">
-            <img src={logo} className="  mr-5" />
+    <header className="container_xxl">
+      <nav className="py-4 flex justify-around items-center  border-b border-gray-300">
+        {/* Logo and Contact Info */}
+        <div className="flex items-center">
+          <img src={logo} className="mr-5" />
 
-            <div className="flex items-center gap-5">
-              <div className="flex">
-                <img src={mobile} alt="mobile" className="mr-2" />
-                <span className="text-darkPrimary text-base ">
-                  8-800-333-6784
-                </span>
-              </div>
+          <div className="flex items-center gap-5">
+            <div className="flex items-center">
+              <img src={mobile} className="mr-2" />
+              <span className="text-darkPrimary text-base">8-800-333-6784</span>
+            </div>
 
-              <div className="flex">
-                <img src={mail} alt="mail" className="mr-2" />
-                <span className="text-darkPrimary text-base ">
-                  info@madex.ru
-                </span>
-              </div>
+            <div className="flex items-center">
+              <img src={mail} className="mr-2" />
+              <span className="text-darkPrimary text-base">info@madex.ru</span>
+            </div>
 
-              <div className="flex">
-                <img src={location} alt="location" className="mr-2" />
-                <span className="text-darkPrimary text-base">Москва</span>
-              </div>
+            <div className="flex items-center">
+              <img src={location} className="mr-2" />
+              <span className="text-darkPrimary text-base">Москва</span>
             </div>
           </div>
+        </div>
 
-          {/* City and Minimum Order Info */}
-          <div className="text-darkSecondary hidden md:block ">
-            <span className="ml-4">Минимальный тираж от 30 шт.</span>
+        {/* City and Minimum Order Info */}
+        <div className="text-darkSecondary hidden md:block">
+          <span className="ml-4">Минимальный тираж от 30 шт.</span>
+        </div>
+
+        {/* Navigation Links */}
+        <div className="flex items-center">
+          <a href="#" className="text-darkPrimary hover:text-redPrimary px-2">
+            Доставка
+          </a>
+          <a href="#" className="text-darkPrimary hover:text-redPrimary px-2">
+            Оплата
+          </a>
+          <a href="#" className="text-darkPrimary hover:text-redPrimary px-2">
+            Контакты
+          </a>
+
+          {/* Shopping Cart */}
+          <div className="ml-4 flex items-center">
+            <span className="text-redPrimary text-base px-2">3</span>
+            <span className="text-darkPrimary text-base">14 619,00 Р</span>
           </div>
+        </div>
+      </nav>
 
-          {/* Navigation Links */}
-          <div className="flex items-center">
-            <a href="#" className="text-darkPrimary hover:text-redPrimary px-2">
-              Доставка
-            </a>
-            <a href="#" className="text-darkPrimary hover:text-redPrimary px-2">
-              Оплата
-            </a>
-            <a href="#" className="text-darkPrimary hover:text-redPrimary px-2">
-              Контакты
-            </a>
-
-            {/* Shopping Cart */}
-            <div className="ml-4 flex items-center">
-              <span className="text-redPrimary text-base px-2">3</span>
-              <span className="text-darkPrimary text-base">14 619,00 Р</span>
-            </div>
-          </div>
-        </nav>
-
-        <nav className="   flex justify-around items-center ">
-          {/* <div className="bg-white flex justify-around  "> */}
+      <nav className="flex justify-center items-center py-2 gap-14">
+        <div className="flex justify-around gap-5">
           <button className="px-3 py-1 bg-redPrimary text-white focus:outline-none rounded-lg">
-            {/* Assuming you use Heroicons for icons */}
-            <span className="">☰ Каталог</span>
+            ☰ Каталог
           </button>
           <button className="px-2 py-1 text-darkSecondary focus:outline-none border border-gray-300 rounded-lg">
-            {/* Assuming you use Heroicons for icons */}
-            <span className="">☰ </span>
+            ☰
           </button>
+        </div>
 
-          <div className="flex items-center">
-            <input
-              className="rounded-l-full border-[10px] border-redPrimary py-1 px-2  w-80 text-darkSecondary leading-tight focus:outline-none"
-              id="search"
-              type="text"
-              placeholder="Поиск (Например: термоноски)"
-            />
-            <div className="p-2">
-              <button className="bg-redPrimary text-white rounded-e-full p-2 focus:outline-none w-12 h-12 flex items-center justify-center">
-                {/* <SearchIcon className="h-5 w-5" /> */}
-              </button>
-            </div>
-          </div>
+        <div className="flex items-center">
+          <input
+            className="rounded-l-full border-[2px] border-redPrimary py-1 px-2 w-[880px] text-darkSecondary leading-tight focus:outline-none"
+            id="search"
+            type="text"
+            placeholder="Поиск (Например: термоноски)"
+          />
+          <button className="bg-redPrimary text-white rounded-r-full p-2 focus:outline-none w-20 h-8 flex items-center justify-center">
+            {/* Search Icon */}
+          </button>
+        </div>
 
-          <div className="flex items-center justify-between text-sm px-2 space-x-4">
-            <span className="text-redPrimary font-semibold">new</span>
-            <span className="text-gray-500">243</span>
-            <span className="text-redPrimary font-semibold">hits</span>
-            <span className="text-gray-500">237</span>
-          </div>
-
-          {/* </div>
-          
-          */}
-        </nav>
-      </header>
-    </>
+        <div className="flex items-center justify-between text-sm px-2 space-x-6">
+          <span className="text-redPrimary font-semibold">
+            new<sup>243</sup>
+          </span>
+          <span className="text-greenPrimary font-semibold">
+            hits<sup>243</sup>
+          </span>
+        </div>
+      </nav>
+    </header>
   );
 };
 
