a4159d0713af5413711dae9f8ec9ca1918c97bd4
Shodmonov01 <a.shodmonov60@gmail.com>
2024-02-14 09:39:11 +0500

    runnigtext3

--- Frontend/src/components/index.ts
+++ Frontend/src/components/index.ts
@@ -1,3 +1,7 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Footer } from "./Footer/Footer";
 export { default as FAQ } from "./FAQ/FAQ";
+export { default as MainCategory } from "./MainCategory/MainCategory";
+export { default as RunningText } from "./RunningText/RunningText";
+
+
