538cffe8dd604362691ef2381be23e49324c12b0
Shodmonov01 <a.shodmonov60@gmail.com>
2024-02-14 09:39:11 +0500

    runnigtext3

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -10,6 +10,10 @@ import {
   ProductNav,
   GiftItem,
 } from "../../components";
+import image1 from "../../assets/article-bg-1.png";
+import image2 from "../../assets/article-bg-2.png";
+import image3 from "../../assets/article-bg-3.png";
+import image4 from "../../assets/article-bg-4.png";
 
 const Home = () => {
   return (
