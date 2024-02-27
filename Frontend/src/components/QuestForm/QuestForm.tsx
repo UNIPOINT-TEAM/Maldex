4a8c58c4392cb8ce98211d5d72b79ac1eab72a28
Shodmonov01 <a.shodmonov60@gmail.com>
2024-02-27 18:21:44 +0500

    quest 50

--- Frontend/src/components/QuestForm/QuestForm.tsx
+++ Frontend/src/components/QuestForm/QuestForm.tsx
@@ -1,17 +1,47 @@
+import React, { useState } from "react";
+import { Input } from "@material-tailwind/react";
+
 function QuestForm() {
-    return (
-        <div>
-            <div className="container_xxl">
-                <div>
-                    <div>
-                        <h1 className="text-fs_2 underline  uppercase text-darkSecondary font-[800]">
-                            хочу задать <br /> вопрос
-                        </h1>
-                    </div>
-                </div>
-            </div>
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsFormVisible((prevState) => !prevState);
+  };
+
+  return (
+    <div>
+      <div className="container_xxl">
+        <div className="mx-10 my-[110px] flex justify-between">
+          <div>
+            <h1
+              className="text-fs_2 underline w-[259px] uppercase text-darkSecondary cursor-pointer font-[800]"
+              onClick={handleClick}
+            >
+              {isFormVisible ? "Ваш Вопрос" : "Хочу задать вопрос"}
+            </h1>
+          </div>
+          <div>
+            {isFormVisible && (
+              <div>
+                <form className="flex flex-col">
+                  <input type="text" placeholder="Ваше имя" />
+                  <input type="text" placeholder="Ваш номер телефона" />
+                  <Input
+                    variant="standard"
+                    label="Standard"
+                    placeholder="Standard"
+                  />
+
+                  <textarea placeholder="Ваш вопрос" />
+                  <button type="submit">Отправить</button>
+                </form>
+              </div>
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default QuestForm;
--- Frontend/src/pages/Delivery/Delivery.tsx
+++ Frontend/src/pages/Delivery/Delivery.tsx
@@ -1,110 +1,137 @@
 import React, { useState } from "react";
-import { QuestForm } from "../../components";
 
 function Delivery() {
-    const [selectedButton, setSelectedButton] = useState(1);
+  const [selectedButton, setSelectedButton] = useState(1);
+  //@ts ignor
+  const handleButtonClick = (buttonIndex) => {
+    setSelectedButton(buttonIndex);
+  };
 
-    const handleButtonClick = (buttonIndex) => {
-        setSelectedButton(buttonIndex);
-    };
+  return (
+    <>
+      <div>
+        <div className="container_xxl">
+          <div className="mx-10">
+            <button
+              className={`px-4 py-2 mr-2 ${
+                selectedButton === 1
+                  ? " text-redPrimary border border-x-[#fff] border-t-[#fff]  border-b-redPrimary "
+                  : " text-gray-700"
+              }`}
+              onClick={() => handleButtonClick(1)}
+            >
+              доставка
+            </button>
+            <button
+              className={`px-4 py-2 mr-2 ${
+                selectedButton === 2
+                  ? " text-redPrimary border border-x-[#fff] border-t-[#fff]  border-b-redPrimary "
+                  : " text-gray-700"
+              }`}
+              onClick={() => handleButtonClick(2)}
+            >
+              оплата
+            </button>
+            <button
+              className={`px-4 py-2 ${
+                selectedButton === 3
+                  ? " text-redPrimary border border-x-[#fff] border-t-[#fff]  border-b-redPrimary "
+                  : " text-gray-700"
+              }`}
+              onClick={() => handleButtonClick(3)}
+            >
+              контакты
+            </button>
 
-    return (
-        <>
-            <div>
-                <div className="container_xxl">
-                    <div className="mx-10">
-                        <button
-                            className={`px-4 py-2 mr-2 ${
-                                selectedButton === 1
-                                    ? "bg-blue-500 text-white"
-                                    : "bg-gray-300 text-gray-700"
-                            }`}
-                            onClick={() => handleButtonClick(1)}
+            <div className="mt-4">
+              {selectedButton === 1 && (
+                <span>
+                  <span>Доставка</span>
+                  Компания «Maldex» бесплатно осуществит доставку заказа и
+                  образцов продукции: По Москве в пределах МКАД До Транспортной
+                  компании Самовывоз доступен в центральном офисе в Москве, по
+                  адресу: м. Нагатинская, Варшавское шоссе 35 стр 1, тел.
+                  8-800-777-59-19
+                </span>
+              )}
+              {selectedButton === 2 && (
+                <span>
+                  Оплата Мы работаем с юридическими и физическими лицами по
+                  безналичному и наличному расчету. Минимальная сумма заказа
+                  составляет 30 000 рублей. При оформлении заказа менеджер
+                  ответит на вопросы, рассчитает стоимость, уточнит детали
+                  заказа и выставит счет на оплату. Отчетные документы: акт
+                  выполненных работ, счет-фактуру, договор мы подготовим и
+                  предоставим при отгрузке заказа.
+                </span>
+              )}
+              <div className="">
+                <div>
+                  {selectedButton === 3 && (
+                    <div className="flex ">
+                      <div className="w-1/4">
+                        <span>
+                          <p className="font-extrabold">Наши контакты</p>
+                          <p>Наш телефон:</p>
+                          <p>8 (800) 777-59-19</p>
+                          <p className="mb-4">8 (495) 760-83-28</p>
+                        </span>
+                        <span>
+                          <p>Email:</p>
+                          <p className="mb-4"> info@maldex.ru</p>
+                        </span>
+                        <span>
+                          <p>Адрес:</p>
+                          <p> Москва, Варшавское шоссе, 35, стр.1</p>
+                        </span>
+                      </div>
+                      <div
+                        className="w-3/4"
+                        style={{
+                          position: "relative",
+                          overflow: "hidden",
+                        }}
+                      >
+                        <a
+                          href="https://yandex.uz/maps/10330/bukhara/?utm_medium=mapframe&utm_source=maps"
+                          style={{
+                            color: "#eee",
+                            fontSize: "12px",
+                            position: "absolute",
+                            top: "0px",
+                          }}
                         >
-                            доставка
-                        </button>
-                        <button
-                            className={`px-4 py-2 mr-2 ${
-                                selectedButton === 2
-                                    ? "bg-blue-500 text-white"
-                                    : "bg-gray-300 text-gray-700"
-                            }`}
-                            onClick={() => handleButtonClick(2)}
+                          Buxoro
+                        </a>
+                        <a
+                          href="https://yandex.uz/maps/geo/771273395/?ll=64.439753%2C39.774226&utm_medium=mapframe&utm_source=maps&z=12.66"
+                          style={{
+                            color: "#eee",
+                            fontSize: "12px",
+                            position: "absolute",
+                            top: "14px",
+                          }}
                         >
-                            оплата
-                        </button>
-                        <button
-                            className={`px-4 py-2 ${
-                                selectedButton === 3
-                                    ? "bg-blue-500 text-white"
-                                    : "bg-gray-300 text-gray-700"
-                            }`}
-                            onClick={() => handleButtonClick(3)}
-                        >
-                            контакты
-                        </button>
-
-                        <div className="mt-4">
-                            {selectedButton === 1 && (
-                                <span>
-                                    <span>Доставка</span>
-                                    Компания «Maldex» бесплатно осуществит
-                                    доставку заказа и образцов продукции: По
-                                    Москве в пределах МКАД До Транспортной
-                                    компании Самовывоз доступен в центральном
-                                    офисе в Москве, по адресу: м. Нагатинская,
-                                    Варшавское шоссе 35 стр 1, тел.
-                                    8-800-777-59-19
-                                </span>
-                            )}
-                            {selectedButton === 2 && (
-                                <span>
-                                    Оплата Мы работаем с юридическими и
-                                    физическими лицами по безналичному и
-                                    наличному расчету. Минимальная сумма заказа
-                                    составляет 30 000 рублей. При оформлении
-                                    заказа менеджер ответит на вопросы,
-                                    рассчитает стоимость, уточнит детали заказа
-                                    и выставит счет на оплату. Отчетные
-                                    документы: акт выполненных работ,
-                                    счет-фактуру, договор мы подготовим и
-                                    предоставим при отгрузке заказа.
-                                </span>
-                            )}
-                            <div className="">
-                                <div>
-                                    {selectedButton === 3 && (
-                                        <div>
-                                            <p>
-                                                Наши контакты Наш телефон: 8
-                                                (800) 777-59-19 8 (495)
-                                                760-83-28 Email: info@maldex.ru
-                                                Адрес: Москва, Варшавское шоссе,
-                                                35, стр.1
-                                            </p>
-                                        </div>
-                                    )}
-                                </div>
-                                <div>
-                                    {selectedButton === 3 && (
-                                        <YMaps>
-                                            <Map
-                                                defaultState={{
-                                                    center: [55.75, 37.57],
-                                                    zoom: 9,
-                                                }}
-                                            />
-                                        </YMaps>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
+                          Buxoro — Yandex Xarita
+                        </a>
+                        <iframe
+                          src="https://yandex.uz/map-widget/v1/?ll=64.439753%2C39.774226&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgk3NzEyNzMzOTUSFE_Ku3piZWtpc3RvbiwgQnV4b3JvIgoN7NeAQhVmEh9C&z=12.66"
+                          width="100%"
+                          height="246"
+                          frameBorder="1"
+                          allowFullScreen={true}
+                          style={{ position: "relative" }}
+                        ></iframe>
+                      </div>
                     </div>
                 </div>
-                <QuestForm />
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Delivery;
