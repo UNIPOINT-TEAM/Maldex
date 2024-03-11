ec30b79db674e1c072dae1a09b4652b8c0c4d7f0
naimovdavlatshoh <you@example.com>
2024-03-11 17:04:27 +0500

    added naimov works

--- Frontend/src/pages/CardSet/CardSet.tsx
+++ Frontend/src/pages/CardSet/CardSet.tsx
@@ -3,7 +3,7 @@ import { CardCatalog, SaleSlider } from "../../components";
 import { CardCatalogData } from "../../mock/data";
 import Photo1 from "../../assets/images/catalog1.png";
 import Photo2 from "../../assets/images/catalog2.png";
-import Photo3 from "../../assets/images/setproduct.png";
+import CarouselImg from "../../assets/images/carouselImg.png";
 
 const CardSet = () => {
     const [mainPhoto, setMainPhoto] = useState(Photo1);
@@ -23,7 +23,7 @@ const CardSet = () => {
         <>
             <div className="home mb-5">
                 <div className="border-b-2 py-3">
-                    <div className="container_xxl">
+                    <div className="container_xxl hidden md:block">
                         <p className="m-0 p-0 text-gray-500">
                             Промо одежда / Портативные колонки / Беспроводная
                             колонка Chubby /{" "}
@@ -33,10 +33,10 @@ const CardSet = () => {
                         </p>
                     </div>
                 </div>
-                <div className="card container_xxl flex">
-                    <div className="w-[75%]">
+                <div className="card container_xxl flex flex-col sm:flex-row">
+                    <div className="w-full md:w-[75%]">
                         <div className="flex">
-                            <div className="h-[400px] w-1/2 flex flex-col justify-center gap-5 p-5">
+                            <div className="h-[400px] w-1/2 hidden md:flex flex-col justify-center gap-5 p-5">
                                 <div>
                                     <p className="text-black text lg">
                                         Изменение состава
@@ -67,8 +67,8 @@ const CardSet = () => {
                                     </p>
                                 </div>
                             </div>
-                            <div className="w-1/2 flex h-[400px]">
-                                <div className="w-[80%] h-full relative">
+                            <div className="w-full flex h-[400px] sm:w-1/2">
+                                <div className="w-full sm:w-[80%] h-full relative z-30">
                                     <img
                                         className="w-full h-full absolute"
                                         src={mainPhoto}
@@ -91,8 +91,42 @@ const CardSet = () => {
                                             примеры
                                         </button>
                                     </div>
+                                    <div className="w-[20%] sm:hidden right-0 top-[100px] absolute flex flex-col justify-center items-center p-3 gap-1 z-50">
+                                        <button
+                                            className={`h-[80px] w-[80px] ${
+                                                activeButton === "photo1"
+                                                    ? "border-2 border-redPrimary"
+                                                    : ""
+                                            }`}
+                                            onClick={() =>
+                                                handleButtonClick(Photo1)
+                                            }
+                                        >
+                                            <img
+                                                className="w-full h-full"
+                                                src={Photo1}
+                                                alt="no img"
+                                            />
+                                        </button>
+                                        <button
+                                            className={`w-[80px] h-[80px] ${
+                                                activeButton === "photo2"
+                                                    ? "border-2 border-redPrimary"
+                                                    : ""
+                                            }`}
+                                            onClick={() =>
+                                                handleButtonClick(Photo2)
+                                            }
+                                        >
+                                            <img
+                                                className="w-full h-full"
+                                                src={Photo2}
+                                                alt="no img"
+                                            />
+                                        </button>
+                                    </div>
                                 </div>
-                                <div className="w-[20%] bg-gray-100 flex flex-col justify-center items-center p-3 gap-1">
+                                <div className="w-[20%] hidden sm:flex bg-gray-100 flex-col justify-center items-center p-3 gap-1">
                                     <button
                                         className={`h-[80px] w-[80px] ${
                                             activeButton === "photo1"
@@ -129,17 +163,17 @@ const CardSet = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="w-[25%] pl-[50px]">
-                        <div className="border-l-2 pl-5 py-3 mb-5">
-                            <p className="text-xl mb-3">
+                    <div className="w-full sm:w-[25%] pl-0 sm:pl-[50px] ">
+                        <div className="border-0 sm:border-l-2 pl-5 py-3 mb-5">
+                            <p className="text-xl my-3">
                                 Ваш набор <span>Майя</span>
                             </p>
-                            <div className="flex flex-col justify-center">
+                            <div className="flex flex-wrap sm:flex-col sm:flex-nowrap justify-start">
                                 <div className="setProduct flex justify-between items-center py-5 border-b-2 ">
-                                    <div className="w-[90%] flex justify-start items-center gap-3">
+                                    <div className="w-1/2 sm:w-[90%] block sm:flex justify-start items-center gap-3">
                                         <img
-                                            className="w-[50px]"
-                                            src={Photo3}
+                                            className="w-full sm:w-[50px] object-contain"
+                                            src={CarouselImg}
                                             alt=""
                                         />
                                         <div className="flex flex-col gap-1">
@@ -150,83 +184,15 @@ const CardSet = () => {
                                                 15 185.55 ₽
                                             </p>
                                         </div>
-                                    </div>
-                                    <div className="flex flex-col justify-center items-center gap-2 setBtns">
-                                        <button>
-                                            <i className="fa-solid fa-xmark text-gray-400"></i>
-                                        </button>
-                                        <button>
-                                            <i className="fa-solid fa-plus text-gray-400"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="setProduct flex justify-between items-center py-5 border-b-2 ">
-                                    <div className="w-[90%] flex justify-start items-center gap-3">
-                                        <img
-                                            className="w-[50px]"
-                                            src={Photo3}
-                                            alt=""
-                                        />
-                                        <div className="flex flex-col gap-1">
-                                            <p className="text-xs">
-                                                Инновационный <br /> очиститель
-                                            </p>
-                                            <p className="text-xs">
-                                                15 185.55 ₽
-                                            </p>
+                                        <div className="flex justify-between items-center py-3 sm:hidden gap-3">
+                                            <p>Количество</p>
+                                            <button className="border-2 border-gray-800 px-4 rounded flex justify-center items-center">
+                                                20
+                                            </button>
                                         </div>
                                     </div>
-                                    <div className="flex flex-col justify-center items-center gap-2 setBtns">
-                                        <button>
-                                            <i className="fa-solid fa-xmark text-gray-400"></i>
-                                        </button>
-                                        <button>
-                                            <i className="fa-solid fa-plus text-gray-400"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="setProduct flex justify-between items-center py-5 border-b-2 ">
-                                    <div className="w-[90%] flex justify-start items-center gap-3">
-                                        <img
-                                            className="w-[50px]"
-                                            src={Photo3}
-                                            alt=""
-                                        />
-                                        <div className="flex flex-col gap-1">
-                                            <p className="text-xs">
-                                                Инновационный <br /> очиститель
-                                            </p>
-                                            <p className="text-xs">
-                                                15 185.55 ₽
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col justify-center items-center gap-2 setBtns">
-                                        <button>
-                                            <i className="fa-solid fa-xmark text-gray-400"></i>
-                                        </button>
-                                        <button>
-                                            <i className="fa-solid fa-plus text-gray-400"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="setProduct flex justify-between items-center py-5 border-b-2 ">
-                                    <div className="w-[90%] flex justify-start items-center gap-3">
-                                        <img
-                                            className="w-[50px]"
-                                            src={Photo3}
-                                            alt=""
-                                        />
-                                        <div className="flex flex-col gap-1">
-                                            <p className="text-xs">
-                                                Инновационный <br /> очиститель
-                                            </p>
-                                            <p className="text-xs">
-                                                15 185.55 ₽
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col justify-center items-center gap-2 setBtns">
+
+                                    <div className=" justify-center items-center gap-2 setBtns hidden sm:flex sm:flex-col">
                                         <button>
                                             <i className="fa-solid fa-xmark text-gray-400"></i>
                                         </button>
