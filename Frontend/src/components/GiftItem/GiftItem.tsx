a67466b783f9b598aa7896712f3282b780300cc1
Shodmonov01 <a.shodmonov60@gmail.com>
2024-03-19 14:58:17 +0500

    giftitem

--- Frontend/src/components/GiftItem/GiftItem.tsx
+++ Frontend/src/components/GiftItem/GiftItem.tsx
@@ -32,31 +32,32 @@ const GiftItem = () => {
         }}
         className="w-full"
       >
-        {response.map((item) => (
-          <SwiperSlide className="flex justify-center">
-            <div className=" flex w-[70px] items-center justify-center">
-              <Link
-                to={"/catalog"}
-                className=" flex items-center flex-col justify-center gap-1 "
-              >
-                <div className="border w-[70px] h-[70px] border-lightPrimary p-3 rounded-xl hover:bg-white duration-300">
-                  <img
-                    src={item.logo}
-                    alt={item.name}
-                    className="w-full h-full object-contain "
-                  />
-                </div>
-                <p className="text-center px-1 text-fs_8 font-semibold font-helvetica-neue-black-condensed">
-                  {item.name}
-                </p>
-              </Link>
-            </div>
-            <p className="text-center px-1 text-[11px] font-medium font-helvetica-neue-black-condensed">
+        {response &&
+          response.map((item) => (
+            <SwiperSlide className="flex justify-center" key={item.id}>
+              <div className=" flex w-[70px] items-center justify-center">
+                <Link
+                  to={"/catalog"}
+                  className=" flex items-center flex-col justify-center gap-1 "
+                >
+                  <div className="border w-[70px] h-[70px] border-lightPrimary p-3 rounded-xl hover:bg-white duration-300">
+                    <img
+                      src={item.logo}
+                      alt={item.name}
+                      className="w-full h-full object-contain "
+                    />
+                  </div>
+                  <p className="text-center px-1 text-fs_8 font-semibold font-helvetica-neue-black-condensed">
+                    {item.name}
+                  </p>
+                </Link>
+              </div>
+              {/* <p className="text-center px-1 text-[11px] font-medium font-helvetica-neue-black-condensed">
               {item.name}
-            </p>
-          </Link>
-        </div>
-      ))}
+            </p> */}
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
