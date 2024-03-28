ba4e3fd515c48b3b9c1340a89d7615e6393a041b
Jobir <jobirashurov7877@gmail.com>
2024-03-28 18:22:37 +0500

    commited

--- Dashboard/src/pages/Main/Main.tsx
+++ Dashboard/src/pages/Main/Main.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import DefaultLayout from '../../layout/DefaultLayout';
 import { GiftItem } from '../../components';
-import MainBanner from '../../components/MainBanner/MainBanner';
 import ProductNav from '../../components/ProductNav/ProductNav';
 import SliderProduct from '../../components/SliderProduct/SliderProduct';
 
 import { Dishes, Faq, MainCatalog } from '../../components';
+import MainBanner from '../../components/MainBanner/MainBanner';
 const Main: React.FC = () => {
   return (
     <DefaultLayout>
+      <div className=" my-4">
+        <GiftItem />
+      </div>
+      <div className=" my-4">
+        <MainBanner />
+      </div>
       <div className="container_xxl px-3 mt-10">
         <MainCatalog />
       </div>
