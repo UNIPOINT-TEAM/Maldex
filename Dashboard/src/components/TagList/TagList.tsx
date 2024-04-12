90e2817ab4d7a48b5af43b485603cc00b8569714
Shodmonov01 <a.shodmonov60@gmail.com>
2024-04-12 10:20:05 +0500

    Taglist edit buttons

--- Dashboard/src/components/TagList/TagList.tsx
+++ Dashboard/src/components/TagList/TagList.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import { Button } from '@material-tailwind/react';
+import { useState } from 'react';
+import { MdEdit, MdDelete } from 'react-icons/md';
 
 const TagList = () => {
-  const [selectedCategory, setSelectedCategory] = useState('Для неё');
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedSubcategory, setSelectedSubcategory] = useState(null);
   const [hoveredCategory, setHoveredCategory] = useState('');
   const [hoveredSubcategory, setHoveredSubcategory] = useState('');
+
+  const [isHovered, setIsHovered] = useState(false);
 
   const [categories, setCategories] = useState({
     'Для неё': [
@@ -24,6 +28,10 @@ const TagList = () => {
 
   const [newCategory, setNewCategory] = useState('');
   const [newSubcategory, setNewSubcategory] = useState('');
+  const [editingCategory, setEditingCategory] = useState(null);
+  const [editingSubcategory, setEditingSubcategory] = useState(null);
+  const [editCategoryInput, setEditCategoryInput] = useState('');
+  const [editSubcategoryInput, setEditSubcategoryInput] = useState('');
 
   const addCategory = () => {
     if (newCategory && !categories[newCategory]) {
@@ -51,28 +59,97 @@ const TagList = () => {
   };
 
   const deleteSubcategory = (subcategory) => {
-    const updatedSubcategories = categories[selectedCategory].filter((sub) => sub !== subcategory);
+    const updatedSubcategories = categories[selectedCategory].filter(
+      (sub) => sub !== subcategory,
+    );
     setCategories({ ...categories, [selectedCategory]: updatedSubcategories });
     setSelectedSubcategory(null);
   };
 
+  const startEditingCategory = (category) => {
+    setEditingCategory(category);
+    setEditCategoryInput(category);
+  };
+
+  const startEditingSubcategory = (subcategory) => {
+    setEditingSubcategory(subcategory);
+    setEditSubcategoryInput(subcategory);
+  };
+
+  const handleEditCategory = () => {
+    if (editCategoryInput && editingCategory && categories[editingCategory]) {
+      const updatedCategories = {
+        ...categories,
+        [editCategoryInput]: categories[editingCategory],
+      };
+      delete updatedCategories[editingCategory];
+      setCategories(updatedCategories);
+      setEditingCategory(null);
+    }
+  };
+
+  const handleEditSubcategory = () => {
+    if (
+      editSubcategoryInput &&
+      editingSubcategory &&
+      categories[selectedCategory] &&
+      categories[selectedCategory].includes(editingSubcategory)
+    ) {
+      const updatedSubcategories = categories[selectedCategory].map((sub) =>
+        sub === editingSubcategory ? editSubcategoryInput : sub,
+      );
+      setCategories({
+        ...categories,
+        [selectedCategory]: updatedSubcategories,
+      });
+      setEditingSubcategory(null);
+    }
+  };
+
+  const cancelEdit = (type) => {
+    if (type === 'category') {
+      setEditingCategory(null);
+    } else if (type === 'subcategory') {
+      setEditingSubcategory(null);
+    }
+  };
+
   return (
     <div className="container_xxl">
-      <div className="flex flex-col ">
-        <div className="mt-4">
-          <input
-            type="text"
-            value={newCategory}
-            onChange={(e) => setNewCategory(e.target.value)}
-            placeholder="Новая категория"
-            className="border p-2"
-          />
-          <button onClick={addCategory} className="ml-2 p-2 border bg-blue-500 text-white">
-            Добавить категорию
-          </button>
+      <div className="flex flex-wrap flex-col ">
+        <div className="mt-4 flex items-center justify-center gap-5">
+          <div className=" ">
+            <input
+              type="text"
+              value={newCategory}
+              onChange={(e) => setNewCategory(e.target.value)}
+              placeholder="Новая категория"
+              className="border p-2 rounded-lg"
+            />
+          </div>
+
+          <div>
+            <Button
+              color="green"
+              // buttonType="filled"
+              // size="regular"
+              rounded={false}
+              block={false}
+              iconOnly={false}
+              // ripple="light"
+              onClick={addCategory}
+            >
+              Добавить
+            </Button>
+          </div>
         </div>
+
         <div className="">
-          <ul className="mt-10 mb-6 justify-around hidden lg:flex">
+          <ul
+            className="mt-10 mb-6 justify-around  flex flex-wrap"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
             {Object.keys(categories).map((category) => (
               <div
                 key={category}
@@ -80,76 +157,176 @@ const TagList = () => {
                 onMouseLeave={() => setHoveredCategory('')}
                 className="relative"
               >
-                <li
-                  onClick={() => setSelectedCategory(category)}
-                  className={`cursor-pointer py-2 px-10 border rounded-3xl ${
-                    selectedCategory === category ? 'font-bold bg-red-primary text-white' : 'bg-white'
-                  }`}
-                >
-                  {category}
-                </li>
-                {hoveredCategory === category && (
-                  <div className="absolute top-0 right-0 flex">
-                    <button className="p-1 text-sm text-white bg-blue-500">Edit</button>
-                    <button
-                      onClick={() => deleteCategory(category)}
-                      className="p-1 text-sm text-white bg-red-500"
-                    >
-                      Delete
-                    </button>
+                {editingCategory === category ? (
+                  <div className="">
+                    <div>
+                      <input
+                        type="text"
+                        value={editCategoryInput}
+                        onChange={(e) => setEditCategoryInput(e.target.value)}
+                        className="border p-2 rounded-3xl"
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <button
+                        onClick={handleEditCategory}
+                        className="ml-2 p-2 border rounded-lg bg-blue-500 text-white"
+                      >
+                        Сохранить
+                      </button>
+                      <button
+                        onClick={() => cancelEdit('category')}
+                        className="ml-2 p-2 border rounded-lg  bg-red-500 text-white"
+                      >
+                        Отмена
+                      </button>
+                    </div>
                   </div>
+                ) : (
+                  <>
+                    <li
+                      onClick={() => setSelectedCategory(category)}
+                      className={`cursor-pointer py-2 px-10 border rounded-3xl my-5 ${
+                        selectedCategory === category
+                          ? 'font-bold bg-red-primary text-white'
+                          : 'bg-white'
+                      } ${
+                        isHovered && hoveredCategory === category ? 'blur' : ''
+                      }`}
+                    >
+                      {category}
+                    </li>
+                    <div className="justify-center items-center gap-2 flex mt-2 hover:opacity-100 opacity-0">
+                      <button
+                        onClick={() => startEditingCategory(category)}
+                        className="shadow-lg rounded-lg p-2 text-sm text-white bg-yellow-400"
+                        style={{
+                          opacity: editingCategory === category ? 0 : 1,
+                        }}
+                      >
+                        <MdEdit />
+                      </button>
+                      <button
+                        onClick={() => deleteCategory(category)}
+                        className="shadow-lg rounded-lg p-2 text-sm text-white bg-red-500"
+                        style={{
+                          opacity: editingCategory === category ? 0 : 1,
+                        }}
+                      >
+                        <MdDelete />
+                      </button>
+                    </div>
+
+                   
+                  </>
                 )}
               </div>
             ))}
           </ul>
-        </div>
-        {selectedCategory && (
-          <div className="">
-            <div className="mt-4">
-              <input
-                type="text"
-                value={newSubcategory}
-                onChange={(e) => setNewSubcategory(e.target.value)}
-                placeholder="Новый тег"
-                className="border p-2"
-              />
-              <button onClick={addSubcategoryToCategory} className="ml-2 p-2 border bg-blue-500 text-white">
-                Добавить тег
-              </button>
-            </div>
-            <ul className="mb-10 flex flex-wrap gap-y-5 justify-between hidden lg:flex">
-              {categories[selectedCategory].map((subcategory, index) => (
-                <div
-                  key={index}
-                  onMouseEnter={() => setHoveredSubcategory(subcategory)}
-                  onMouseLeave={() => setHoveredSubcategory('')}
-                  className="relative"
-                  style={{ width: '18%' }}
-                >
-                  <li
-                    onClick={() => setSelectedSubcategory(subcategory)}
-                    className={`cursor-pointer py-2 border rounded-xl px-4 text-center ${
-                      selectedSubcategory === subcategory ? 'font-bold bg-red-primary text-white' : 'bg-white'
-                    }`}
-                  >
-                    {subcategory}
-                  </li>
-                  {hoveredSubcategory === subcategory && (
-                    <div className="absolute top-0 right-0 flex">
-                      <button className="p-1 text-sm text-white bg-blue-500">Edit</button>
-                      <button
-                        onClick={() => deleteSubcategory(subcategory)}
-                        className="p-1 text-sm text-white bg-red-500"
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  )}
+          {selectedCategory && (
+            <div className="">
+
+
+              <div className="mb-8 flex items-center justify-center gap-5">
+                <div className=" ">
+                  <input
+                    type="text"
+                    value={newSubcategory}
+                    onChange={(e) => setNewSubcategory(e.target.value)}
+                    placeholder="Новый тег"
+                    className="border p-2 rounded-lg"
+                  />
                 </div>
-              ))}
-            </ul>
-          </div>
-        )}
+
+                <div>
+                  <Button
+                    color="green"
+                    // buttonType="filled"
+                    // size="regular"
+                    rounded={false}
+                    block={false}
+                    iconOnly={false}
+                    // ripple="light"
+                    onClick={addSubcategoryToCategory}
+                  >
+                    Добавить
+                  </Button>
+                </div>
+              </div>
+              <ul className="mb-10 flex flex-wrap gap-y-5 justify-between hidden lg:flex">
+                {categories[selectedCategory].map((subcategory, index) => (
+                  <div
+                    key={index}
+                    onMouseEnter={() => setHoveredSubcategory(subcategory)}
+                    onMouseLeave={() => setHoveredSubcategory('')}
+                    className="relative"
+                    style={{ width: '18%' }}
+                  >
+                    {editingSubcategory === subcategory ? (
+                      <div className="">
+                        <div>
+                          <input
+                            type="text"
+                            value={editSubcategoryInput}
+                            onChange={(e) =>
+                              setEditSubcategoryInput(e.target.value)
+                            }
+                            className="border p-2 rounded-lg"
+                          />
+                        </div>
+                        <div className='flex justify-start mt-2'>
+                          <button
+                            onClick={handleEditSubcategory}
+                            className="ml-2 p-2 border bg-blue-500 text-white"
+                          >
+                            Сохранить
+                          </button>
+                          <button
+                            onClick={() => cancelEdit('subcategory')}
+                            className="ml-2 p-2 border bg-red-500 text-white"
+                          >
+                            Отмена
+                          </button>
+                        </div>
+                      </div>
+                    ) : (
+                      <>
+                        <li
+                          onClick={() => setSelectedSubcategory(subcategory)}
+                          className={`cursor-pointer py-2 border rounded-xl px-4 text-center ${
+                            selectedSubcategory === subcategory
+                              ? 'font-bold bg-red-primary text-white'
+                              : 'bg-white'
+                          }`}
+                        >
+                          {subcategory}
+                        </li>
+                        {hoveredSubcategory === subcategory && (
+                          <div className=" justify-center items-center gap-2 flex mt-2">
+                            <button
+                              onClick={() =>
+                                startEditingSubcategory(subcategory)
+                              }
+                              className="shadow-lg rounded-lg p-2 text-sm text-white bg-yellow-400"
+                            >
+                              <MdEdit />
+                            </button>
+                            <button
+                              onClick={() => deleteSubcategory(subcategory)}
+                              className="shadow-lg rounded-lg p-2 text-sm text-white bg-red-500"
+                            >
+                              <MdDelete />
+                            </button>
+                          </div>
+                        )}
+                      </>
+                    )}
+                  </div>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- Dashboard/src/pages/BuildSet/BuildSet.tsx
+++ Dashboard/src/pages/BuildSet/BuildSet.tsx
@@ -4,166 +4,185 @@ import {
   AccordionBody,
   AccordionHeader,
   Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
 } from '@material-tailwind/react';
 import accordionIcon from '../../assets/icons/accordion-icon.png';
 import { SliderProduct } from '../../components';
-// import GiftBanner from '../../assets/gift_builder_banner.png';
-import ProductCart from '../../assets/images/machine.png';
 import { IoAddSharp, IoCloseSharp } from 'react-icons/io5';
 import DefaultLayout from '../../layout/DefaultLayout';
+import { MdDelete, MdEdit } from 'react-icons/md';
 
 const BuildSet = () => {
   const [open, setOpen] = useState<number>(0);
-  // @ts-ignore
-  const [buildCart, setBuildCart] = useState([]);
-  const [quantityVisible, setQuantityVisible] = useState<boolean>(false);
-  // @ts-ignore
-  const [isDelete, setIsDelete] = useState<boolean>(false);
+  const [buildCart, setBuildCart] = useState<any[]>([]);
+  const [accordionCount, setAccordionCount] = useState<number>(5);
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [editedAccordionIndex, setEditedAccordionIndex] = useState<
+    number | null
+  >(null);
+  const [editedAccordionTitle, setEditedAccordionTitle] = useState<string>('');
+
   const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
+
+  const addAccordion = () => {
+    setAccordionCount((prevCount) => prevCount + 1);
+  };
+
+  const editAccordion = (index: number, event: React.MouseEvent) => {
+    event.stopPropagation(); // Остановить распространение события
+    setIsDialogOpen({
+      isOpen: true,
+      editedIndex: index,
+      editedTitle: `Аккордеон ${index + 1}`,
+    });
+  };
+
+  const saveEditedAccordion = () => {
+    const updatedAccordions = [...buildCart];
+    updatedAccordions[editedAccordionIndex!] = {
+      ...updatedAccordions[editedAccordionIndex!],
+      title: editedAccordionTitle,
+    };
+    setBuildCart(updatedAccordions);
+    setIsDialogOpen(false);
+  };
+
+  const deleteAccordion = (index: number) => {
+    const updatedAccordions = buildCart.filter((_, idx) => idx !== index);
+    setBuildCart(updatedAccordions);
+    setAccordionCount((prevCount) => prevCount - 1);
+  };
+
   return (
     <DefaultLayout>
       <div className="">
         <div className="grid grid-cols-10">
           <div className="col-span-10 lg:col-span-10">
             <div className="bg-greenPrimary h-[75px] flex items-center justify-center ">
-              <h1 className=" text-[22px] lg:text-[30px] text-red-primary">
+              <h1 className="text-[22px] lg:text-[30px] text-red-primary">
                 Создайте идеальный подарок
               </h1>
             </div>
-            <div className="w-full">
-              <Accordion
-                className=" border px-5 border-lightPrimary my-4"
-                open={open === 1}
-                icon={
-                  <img
-                    className={`${
-                      open === 1 ? 'rotate-180' : ''
-                    } transition-transform w-[18px]`}
-                    src={accordionIcon}
-                  />
-                }
-                placeholder={<div />}
-              >
-                <AccordionHeader
-                  className="border-0  p-4"
-                  onClick={() => handleOpen(1)}
-                  placeholder={<div />}
+            <div className="w-[96%]">
+              {[...Array(accordionCount)].map((_, index) => (
+                <>
+                  <div className="flex item-center">
+                    <Accordion
+                      key={index}
+                      className="border border-lightPrimary px-5 my-4"
+                      open={open === index + 1}
+                      icon={
+                        <img
+                          className={`${
+                            open === index + 1 ? 'rotate-180' : ''
+                          } transition-transform w-[18px]`}
+                          src={accordionIcon}
+                        />
+                      }
+                      placeholder={<div />}
+                    >
+                      <AccordionHeader
+                        className="border-0 p-4"
+                        onClick={() => handleOpen(index + 1)}
+                        placeholder={<div />}
+                      >
+                        <h2 className="font-helvetica tracking-wide text-fs_6 font-normal text-greenPrimary">
+                          {index + 1}. Заголовок аккордеона
+                        </h2>
+                      </AccordionHeader>
+                      <AccordionBody className="p-4" placeholder={<div />}>
+                        <SliderProduct />
+                      </AccordionBody>
+                    </Accordion>
+                    <div className="flex flex-col justify-center mt-2">
+                      <Button
+                        // color="blue"
+                        buttonType="filled"
+                        size="regular"
+                        rounded={false}
+                        block={false}
+                        iconOnly={true}
+                        ripple="light"
+                        onClick={(event) => editAccordion(index, event)}
+                        className="bg-yellow-400"
+                      >
+                        <MdEdit />
+                      </Button>
+
+                      <Button
+                        color="red"
+                        buttonType="filled"
+                        size="regular"
+                        rounded={false}
+                        block={false}
+                        iconOnly={true}
+                        ripple="light"
+                        onClick={() => deleteAccordion(index)}
+                      >
+                        <MdDelete />
+                      </Button>
+                    </div>
+                  </div>
+                </>
+              ))}
+
+              {/* Диалоговое окно для редактирования названия аккордеона */}
+              {isDialogOpen && (
+                <Dialog
+                  size="sm"
+                  open={isDialogOpen}
+                  onClose={() => setIsDialogOpen(false)}
                 >
-                  <h2 className="font-helvetica tracking-wide text-fs_6 font-normal text-greenPrimary ">
-                    1. Ежедневники
-                  </h2>
-                </AccordionHeader>
-                <AccordionBody className="p-4" placeholder={<div />}>
-                  <SliderProduct />
-                </AccordionBody>
-              </Accordion>
-              <Accordion
-                className=" border  border-lightPrimary px-5 my-4"
-                open={open === 2}
-                icon={
-                  <img
-                    className={`${
-                      open === 2 ? 'rotate-180' : ''
-                    } transition-transform w-[18px]`}
-                    src={accordionIcon}
-                  />
-                }
-                placeholder={<div />}
-              >
-                <AccordionHeader
-                  className="border-0  p-4"
-                  onClick={() => handleOpen(2)}
-                  placeholder={<div />}
+                  <DialogHeader>Редактировать название аккордеона</DialogHeader>
+                  <DialogBody>
+                    <input
+                      type="text"
+                      className="w-full p-2 border rounded"
+                      placeholder="Название аккордеона"
+                      value={editedAccordionTitle}
+                      onChange={(e) => setEditedAccordionTitle(e.target.value)}
+                    />
+                  </DialogBody>
+                  <DialogFooter>
+                    <Button
+                      color="blue"
+                      buttonType="link"
+                      onClick={saveEditedAccordion}
+                      ripple="dark"
+                    >
+                      Сохранить
+                    </Button>
+                    <Button
+                      color="red"
+                      buttonType="link"
+                      onClick={() => setIsDialogOpen(false)}
+                      ripple="dark"
+                    >
+                      Закрыть
+                    </Button>
+                  </DialogFooter>
+                </Dialog>
+              )}
+
+              <div className="flex justify-end mt-4">
+                <Button
+                  color="green"
+                  buttonType="filled"
+                  size="regular"
+                  rounded={false}
+                  block={false}
+                  iconOnly={false}
+                  ripple="light"
+                  onClick={addAccordion}
                 >
-                  <h2 className="font-helvetica tracking-wide text-fs_6 font-normal text-greenPrimary ">
-                    2. Термокружки
-                  </h2>
-                </AccordionHeader>
-                <AccordionBody className="p-4" placeholder={<div />}>
-                  <SliderProduct />
-                </AccordionBody>
-              </Accordion>
-              <Accordion
-                className=" border  border-lightPrimary px-5 my-4"
-                open={open === 3}
-                icon={
-                  <img
-                    className={`${
-                      open === 3 ? 'rotate-180' : ''
-                    } transition-transform w-[18px]`}
-                    src={accordionIcon}
-                  />
-                }
-                placeholder={<div />}
-              >
-                <AccordionHeader
-                  className="border-0  p-4"
-                  onClick={() => handleOpen(3)}
-                  placeholder={<div />}
-                >
-                  <h2 className="font-helvetica tracking-wide text-fs_6 font-normal text-greenPrimary ">
-                    3. Гаджеты
-                  </h2>
-                </AccordionHeader>
-                <AccordionBody className="p-4" placeholder={<div />}>
-                  <SliderProduct />
-                </AccordionBody>
-              </Accordion>
-              <Accordion
-                className=" border  border-lightPrimary px-5 my-4"
-                open={open === 4}
-                icon={
-                  <img
-                    className={`${
-                      open === 4 ? 'rotate-180' : ''
-                    } transition-transform w-[18px]`}
-                    src={accordionIcon}
-                  />
-                }
-                placeholder={<div />}
-              >
-                <AccordionHeader
-                  className="border-0  p-4"
-                  onClick={() => handleOpen(4)}
-                  placeholder={<div />}
-                >
-                  <h2 className="font-helvetica tracking-wide text-fs_6 font-normal text-greenPrimary ">
-                    4. Добавьте еще что-то
-                  </h2>
-                </AccordionHeader>
-                <AccordionBody className="p-4" placeholder={<div />}>
-                  <SliderProduct />
-                </AccordionBody>
-              </Accordion>
-              <Accordion
-                className=" border  border-lightPrimary px-5 my-4"
-                open={open === 5}
-                icon={
-                  <img
-                    className={`${
-                      open === 5 ? 'rotate-180' : ''
-                    } transition-transform w-[18px]`}
-                    src={accordionIcon}
-                  />
-                }
-                placeholder={<div />}
-              >
-                <AccordionHeader
-                  className="border-0  p-4"
-                  onClick={() => handleOpen(5)}
-                  placeholder={<div />}
-                >
-                  <h2 className="font-helvetica tracking-wide text-fs_6 font-normal text-greenPrimary ">
-                    5. Выберите упаковку
-                  </h2>
-                </AccordionHeader>
-                <AccordionBody className="p-4" placeholder={<div />}>
-                  <SliderProduct />
-                </AccordionBody>
-              </Accordion>
+                  Добавить
+                </Button>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     </DefaultLayout>
