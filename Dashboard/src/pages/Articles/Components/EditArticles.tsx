802304927413d7224806c0e19d97ad9ca3ca4ddc
Shodmonov01 <a.shodmonov60@gmail.com>
2024-04-25 15:50:24 +0500

    COMMITED 25.04

--- Dashboard/src/pages/Articles/Components/EditArticles.tsx
+++ Dashboard/src/pages/Articles/Components/EditArticles.tsx
@@ -1,15 +1,101 @@
-import React from 'react'
-import DefaultLayout from '../../../layout/DefaultLayout'
+import React, { useState, useEffect } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import DefaultLayout from '../../../layout/DefaultLayout';
+import { GetArticlesDetail, UpgradeArticles } from '../../../services/articles';
 import { useParams } from 'react-router-dom';
+import { Button, Input } from '@material-tailwind/react';
 
 function EditArticles() {
   const { id } = useParams();
-  console.log(id);
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const article = await GetArticlesDetail(Number(id));
+        setTitle(article.title);
+        setContent(article.body);
+      } catch (error) {
+        console.error('Error fetching article:', error);
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  const handleEditorChange = (event, editor) => {
+    const data = editor.getData();
+    setContent(data);
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('title', title);
+      formData.append('body', content);
+      formData.append('image', image);
+
+      await UpgradeArticles(Number(id), formData);
+      console.log('Article updated successfully');
+    } catch (error) {
+      console.error('Error updating article:', error);
+    }
+  };
+
   return (
     <DefaultLayout>
-      <div>EditArticles</div>
+      <div className="my-5">
+        <label
+          // htmlFor={`${index}`}
+          className="flex w-1/2 h-[190px] cursor-pointer border-dashed items-center justify-center gap-2 rounded-xl border border-b py-1 px-2 text-sm font-medium  hover:bg-opacity-90 xsm:px-4"
+        >
+          <input
+            required
+            label="Фото"
+            type="file"
+            // name={`${index}`}
+            // id={`${index}`}
+            className="sr-only"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+          <p className="text-fs-6">Добавить Фото</p>
+        </label>
+      </div>
+      <div className="my-10 w-1/3">
+        <Input
+          value={title}
+          required
+          variant="standard"
+          label="Название"
+          placeholder=""
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div className="w-1/2">
+        <CKEditor
+          editor={ClassicEditor}
+          data={content}
+          onChange={handleEditorChange}
+        />
+      </div>
+      <Button className="my-6" color="blue" onClick={handleSubmit}>
+        Отправить
+      </Button>
     </DefaultLayout>
-  )
+  );
 }
 
-export default EditArticles
+export default EditArticles;
--- Dashboard/src/services/articles.ts
+++ Dashboard/src/services/articles.ts
@@ -5,15 +5,18 @@ export const GetArticles = async () => {
   const response = await api.get(`/articles/`);
   return response.data;
 };
+export const GetArticlesDetail = async (id:number) => {
+  const response = await api.get(`/articles/${id}`);
+  return response.data;
+};
 
 
-// export const PostArticles = async (item:any) => {
-//   const response = await api.post(`/articles/`, item);
-//   return response.data;
-// };
-
-// services/articles.js
 export const PostArticles = async (data:any) => {
   const response = await api.post(`/articles/`, data);
+  return response.data;
+};
+
+export const UpgradeArticles = async (id: number, newData: { title: string, body: string, image: any }) => {
+  const response = await api.put(`/articles/${id}/`, newData); 
   return response.data;
 };
