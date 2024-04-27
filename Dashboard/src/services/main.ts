abc37bc55bde53be756e92af82f7a8f3d34a7818
naimovdavlatshoh <you@example.com>
2024-04-27 15:35:33 +0500

    added giftitem

--- Dashboard/src/services/main.ts
+++ Dashboard/src/services/main.ts
@@ -1,8 +1,7 @@
 import { api } from '../axios/Api';
 
-
 export const GetNewCategory = async () => {
-  const response = await api.get(`/product/categories/?new_category=true`);
+  const response = await api.get(`/product/categories/?is_popular=true`);
   return response.data;
 };
 export const GetMainBanner = async () => {
@@ -19,35 +18,31 @@ export const GetProduct = async () => {
   return response.data;
 };
 
-
-export const GetFaqHome
- = async () => {
+export const GetFaqHome = async () => {
   const response = await api.get(`/faq/?type=home`);
   return response.data;
 };
-
-
 
 export const PostFaqHome = async (data: any) => {
   const response = await api.post(`/faq/?type=home`, data);
   return response.data;
 };
 
-export const delFaqHome = async (id:number)   => {
+export const delFaqHome = async (id: number) => {
   const response = await api.delete(`/faq/${id}`);
   return response.data;
 };
 
-
 //hooks.ts
-export const editFaqHome = async (id: number, newData: { title: string, body: string }) => {
+export const editFaqHome = async (
+  id: number,
+  newData: { title: string; body: string },
+) => {
   const response = await api.put(`/faq/${id}/`, newData);
   return response.data;
 };
 
-
-export const GetPrintCategory
- = async () => {
+export const GetPrintCategory = async () => {
   const response = await api.get(`/print-categories/`);
   return response.data;
 };
