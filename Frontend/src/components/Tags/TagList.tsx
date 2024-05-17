11af503e9ce69d26bb2616dddc68b89945f109cc
Shodmonov01 <a.shodmonov60@gmail.com>
2024-05-17 16:24:34 +0500

    small edit by amir

--- Frontend/src/components/Tags/TagList.tsx
+++ Frontend/src/components/Tags/TagList.tsx
@@ -33,10 +33,10 @@ const TagList = () => {
               <li
                 key={category.id}
                 onClick={() => handleCategoryClick(category)}
-                className={`cursor-pointer py-2 px-10 border rounded-3xl uppercase  ${
+                className={`cursor-pointer font-bold h-[40px] text-fs_8  flex items-center justify-center  px-16  rounded-3xl uppercase  ${
                   selectedCategory === category
-                    ? "font-bold bg-redPrimary text-white"
-                    : "bg-white"
+                    ? " bg-redPrimary text-white"
+                    : "bg-white text-[#666666]"
                 }`}
               >
                 {category.name}
@@ -56,11 +56,11 @@ const TagList = () => {
                   <li
                     key={subcategory.id}
                     onClick={() => handleSubcategoryClick(subcategory)}
-                    className={`cursor-pointer py-2 border rounded-xl px-4 text-center hover:text-white hover:bg-redPrimary hover:font-bold ${
+                    className={`cursor-pointer flex items-center justify-center h-[70px] border border-lightSecondary hover:text-white hover:bg-redPrimary hover:font-bold rounded-xl px-4 text-center ${
                       selectedSubcategory === subcategory
-                        ? "font-bold bg-redPrimary text-white"
-                        : "bg-white"
-                    }`}
+                      ? "font-bold bg-redPrimary text-white"
+                      : "bg-[#fff]"
+                  }`}
                     style={{ width: "18%" }}
                   >
                     {subcategory.name}
